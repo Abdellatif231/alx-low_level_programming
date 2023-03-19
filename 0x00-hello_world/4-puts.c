dd01c37920c3a77d28f8872708a7399895aad1e6
Abdellatif231 <abdomaouni404@gmail.com>
2023-03-19 17:36:26 +0000

    first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * prints somthing using puts
+ *
+ * ruturns: always Success
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}

c432823e1f4b73886017fe46be8b8f72cf39694d
Abdellatif231 <abdomaouni404@gmail.com>
2023-03-21 22:51:13 +0000

    hopfuly

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,31 +6,20 @@
  *
  * Return: Always 0 (Success)
  */
-/**
- * _putchar - Writes the character c to stdout
- * This function writes the character c to the standard output (stdout)
- * @c: The character to be written.
- *Return: On success, the character written is returned. On failure, -1 is returned.
-*/
 
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
 	return (0);
 }

ed627c0238146094f21b706eb87487e21bb9f4e5
Abdellatif231 <abdomaouni404@gmail.com>
2023-03-21 00:10:10 +0000

    last one

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i == 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+
+
+	return (0);
+}

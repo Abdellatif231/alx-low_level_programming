1b329ed14e9e624aedc949055704d131f1d1c2d9
Abdellatif231 <abdomaouni404@gmail.com>
2023-03-24 22:21:37 +0000

    debugging

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * main - Entry point of the program
+  * positive_or_negative - print if number neg or pos
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}

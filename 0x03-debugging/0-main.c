2972d5ab61b3b41c369dc1f9704b7c50d1d473e9
Abdellatif231 <abdomaouni404@gmail.com>
2023-03-25 18:10:45 +0000

    fixed

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,6 @@
-#include <stdio.h>
 #include "main.h"
 /**
   * main - Entry point of the program
-  * positive_or_negative - print if number neg or pos
   *
   * Return: Always 0 (Success)
   */
@@ -12,7 +10,7 @@ int main(void)
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
+
 	return (0);
 }

d39be053f9080fb5b07b91bc6e6e48be3aebab96
Abdellatif231 <abdomaouni404@gmail.com>
2023-03-29 01:30:50 +0000

    prints only half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,7 +13,15 @@ void puts_half(char *str)
 
 	for (n = strlen(str) / 2; str[n] != '\0'; n++)
 	{
-		_putchar(str[n]);
+		if (n % 2 == 0)
+		{
+			_putchar(str[n]);
+		}
+		else
+		{
+			n = (strlen(str) - 1 / 2);
+			_putchar(str[n];
+		}
 	}
 	_putchar('\n');
 }

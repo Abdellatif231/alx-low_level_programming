fa884931e945372424950df9cc374a66af291db6
Abdellatif231 <abdomaouni404@gmail.com>
2023-03-29 00:12:56 +0000

    every other char

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,14 +8,19 @@
 
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
-		if(str[i - 1] != '\0')
-		{	break;
-		}
 		_putchar(str[i]);
+
+		i++;
+
+		if (str[i] = '\0')
+		{
+			break;
+		}
+
 		i++;
 
 	}
